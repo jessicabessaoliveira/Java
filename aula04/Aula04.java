9a22996f9ad88d08f7c2084f292716538ecf4767
Jéssica Bessa de Oliveira <jessicabessaoliveira@gmail.com>
2023-03-29 20:39:12 -0300

    aulas cursoemvideo

---
+++ aula04/Aula04.java
@@ -0,0 +1,13 @@
+package com.mycompany.aula04;
+public class Aula04 {
+    public static void main(String[] args) {
+        Caneta c1 = new Caneta("BIC","AMARELO", 0.4f);
+        c1.status();
+        /*
+        c1.setModelo("BIC");
+        c1.setPonta(0.5f);
+        //c1.status();
+        System.out.println("Tenho uma caneta " + c1.getModelo() + " de ponta " + c1.getPonta());
+    */
+    }
+}
